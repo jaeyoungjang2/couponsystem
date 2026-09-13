e9019f2c05b958a9567c42274a2ce09470a0eae3
jaeyoungjang2 <lion85488@gmail.com>
2026-09-13 21:12:09 +0900

    feat: create coupon entity

--- src/main/kotlin/com/example/coupon/domain/Coupon.kt
+++ src/main/kotlin/com/example/coupon/domain/Coupon.kt
@@ -1,4 +1,30 @@
 package com.example.coupon.domain
 
-class Coupon {
+import jakarta.persistence.*
+import java.time.LocalDateTime
+
+@Entity
+@Table(name = "coupon")
+class Coupon (
+    @Column(nullable = false, length = 80)
+    var name: String,
+
+    @Column(nullable = false)
+    var totalQuantity: Int,
+
+    @Column(nullable = false)
+    var issuedQuantity: Int,
+
+    @Column(nullable = false)
+    var validityDays: Int = 7,
+
+    var startsAt: LocalDateTime? = null,
+
+    @Column(nullable = false, updatable = false)
+    var createdAt: LocalDateTime? = LocalDateTime.now(),
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Long? = null
+    ) {
 }
