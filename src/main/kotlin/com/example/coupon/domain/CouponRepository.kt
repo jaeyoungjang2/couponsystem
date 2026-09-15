691afe21e7ec560554c553fe3d79acf726b04e83
jaeyoungjang2 <lion85488@gmail.com>
2026-09-15 20:02:50 +0900

    add update method

--- src/main/kotlin/com/example/coupon/domain/CouponRepository.kt
+++ src/main/kotlin/com/example/coupon/domain/CouponRepository.kt
@@ -3,6 +3,7 @@ package com.example.coupon.domain
 import jakarta.persistence.LockModeType
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Lock
+import org.springframework.data.jpa.repository.Modifying
 import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.query.Param
 
@@ -11,4 +12,8 @@ interface CouponRepository : JpaRepository<Coupon, Long>{
     @Lock(LockModeType.PESSIMISTIC_WRITE)
     @Query("SELECT c FROM Coupon c WHERE c.id = :id")
     fun findByIdForUpdate(@Param("id") id: Long): Coupon?
+
+    @Modifying
+    @Query("UPDATE Coupon c SET c.issuedQuantity = c.issuedQuantity + 1 WHERE c.id = :id")
+    fun incrementIssuedQuantity(@Param("id") id: Long)
 }
